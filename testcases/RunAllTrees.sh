e69e8606ee0728eeb7151916da0eef0c3b1fdacc
Nils Hölscher <nilhoel1@gmail.com>
2023-03-02 08:59:59 +0000

    Parallel WCET analysis for trees limiter

--- testcases/RunAllTrees.sh
+++ testcases/RunAllTrees.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-search_dir=/workspaces/llvmta/testcases/Benchmarks/trees
-for entry in "$search_dir"/*
-do
-  #result=$(echo "$entry" | sed "s/Suzi/$secondString/")
-  #echo ${entry/trees$second_string}
-  #echo "$entry" | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2-
-  #echo "$entry"
-  for entry2 in "$entry"/*
+
+parse(){
+  search_dir=/workspaces/llvmta/testcases/Benchmarks/trees
+  for entry in "$search_dir"/*
   do
-    for entry3 in "$entry2"/*
+    #result=$(echo "$entry" | sed "s/Suzi/$secondString/")
+    #echo ${entry/trees$second_string}
+    #echo "$entry" | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2-
+    #echo "$entry"
+    for entry2 in "$entry"/*
     do
-      testcase=$(echo "$entry3" | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2-)
-      ./runTestcase "$testcase"  --ta-lpsolver-effort=maximal --ta-pathana-type=graphilp &>/dev/null &
-      echo "$testcase"
+      for entry3 in "$entry2"/*
+      do
+        testcase=( "$(echo "$entry3" | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2- | cut -d'/' -f2-)" )
+        #testcases+=($testcase)
+        #./runTestcase "$testcase"  --ta-lpsolver-effort=maximal --ta-pathana-type=graphilp &>/dev/null &
+        echo "$testcase"
+      done
     done
   done
-done
+}
+
+parse | xargs -P 4 -n 1 -I CMD ./runTestcase CMD  --ta-lpsolver-effort=maximal --ta-pathana-type=graphilp &>/dev/null &
