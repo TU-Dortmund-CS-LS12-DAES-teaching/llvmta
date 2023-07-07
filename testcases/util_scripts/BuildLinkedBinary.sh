4289bac17be7da3d0594fca4b1aa1894a991eed6
joraab <62474014+numberTwenty7@users.noreply.github.com>
2023-07-07 13:58:12 +0000

    added full target to avoid overriding source targ.

--- testcases/util_scripts/BuildLinkedBinary.sh
+++ testcases/util_scripts/BuildLinkedBinary.sh
@@ -4,6 +4,6 @@
 #binary="./dummyBinaryfile.txt"
 #echo "still TODO" >> $binary
 #clang -O0 -c optimized.ll --target=riscv32 -o binary
-clang -O0 -c optimized.ll --target=riscv32 -o bin.o
+clang -O0 -c optimized.ll --target=riscv32-unknown-unknown -o bin.o
 riscv64-unknown-elf-gcc -O0 bin.o -o binary -march=rv32imac -mabi=ilp32
 llvm-objdump binary -d -M numeric > binary.dump
