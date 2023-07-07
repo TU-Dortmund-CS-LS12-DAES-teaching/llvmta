e0156d4bb4ed978f0da2637e64a7c135f49a2ac3
joraab <62474014+numberTwenty7@users.noreply.github.com>
2023-07-07 11:49:45 +0000

    added linking and compiling to actual executable

--- testcases/util_scripts/BuildLinkedBinary.sh
+++ testcases/util_scripts/BuildLinkedBinary.sh
@@ -3,5 +3,7 @@
 
 #binary="./dummyBinaryfile.txt"
 #echo "still TODO" >> $binary
-clang -O0 -c optimized.ll --target=riscv32 -o binary
+#clang -O0 -c optimized.ll --target=riscv32 -o binary
+clang -O0 -c optimized.ll --target=riscv32 -o bin.o
+riscv64-unknown-elf-gcc -O0 bin.o -o binary -march=rv32imac -mabi=ilp32
 llvm-objdump binary -d -M numeric > binary.dump
