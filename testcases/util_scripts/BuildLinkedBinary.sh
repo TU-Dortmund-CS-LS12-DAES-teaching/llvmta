5b751177ebf19c01760c0dd72e40b73f94bc6e1c
joraab <62474014+numberTwenty7@users.noreply.github.com>
2023-11-01 14:10:45 +0000

    added description as comments

--- testcases/util_scripts/BuildLinkedBinary.sh
+++ testcases/util_scripts/BuildLinkedBinary.sh
@@ -1,9 +1,6 @@
-#TODO get target arch (shell script parameters?)
-#TODO generate linked binary
-
-#binary="./dummyBinaryfile.txt"
-#echo "still TODO" >> $binary
-#clang -O0 -c optimized.ll --target=riscv32 -o binary
+#Compile source to object files via LLVM
 clang -O0 -c optimized.ll --target=riscv32-unknown-unknown -o bin.o
+#Compile and Links object files further into exectuable binary
 riscv64-unknown-elf-gcc -O0 bin.o -o binary -march=rv32imac -mabi=ilp32
+#Dissassemble executable and make it accessible in the file binary.dump
 llvm-objdump binary -d -M numeric > binary.dump
